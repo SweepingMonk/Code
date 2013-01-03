ae7799a66aeb5ae0a35cabab16c82069cf31fee3
kongdeqian <kongdeqian0905@gmail.com>
2013-01-03 13:27:58 +0800

    update

--- code.c
+++ code.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 
+void sequence(int t)
+{
+    if(t<3)
+        return;
+    int small,big;
+    int p=0;
+    small=1;
+    big=2;
+    p=small+big;
+    while(small<=(t>>1))
+    {
+        if(p==t)
+        {
+            printf("%d %d\n",small, big);
+            p-=small;
+            small++;
+        }
+        else if(p>t)
+        {
+            p-=small;
+            small++;
+        }
+        else
+        {
+            big++;
+            p+=big;
+        }
+    }
+}
+
 int main()
 {
-    printf("haha\n");
+    int n;
+    scanf("%d",&n);
+    sequence(n);
     return 0;
 }

e58fc80382a1706e9fee3f7018c72bb729bce302
kongdeqian <kongdeqian0905@gmail.com>
2013-01-13 18:42:08 +0800

    update

--- code.c
+++ code.c
@@ -1,39 +1,78 @@
-#include <stdio.h>
+i#include <stdio.h>
+#include <string.h>
+int a[100][100];
+int p[100][100];
 
-void sequence(int t)
+void printRect(int n, int m)
 {
-    if(t<3)
-        return;
-    int small,big;
-    int p=0;
-    small=1;
-    big=2;
-    p=small+big;
-    while(small<=(t>>1))
+    int i=1,j=1;
+    int flag=0;
+    while(!p[i][j])
     {
-        if(p==t)
+        switch(flag)
         {
-            printf("%d %d\n",small, big);
-            p-=small;
-            small++;
+        case 0:
+            for(;j<=m && !p[i][j];j++)
+            {
+                printf("%d ",a[i][j]);
+                p[i][j]=1;
+            }
+            i++;
+            j--;
+            break;
+        case 1:
+            for(;i<=n && !p[i][j];i++)
+            {
+                printf("%d ",a[i][j]);
+                p[i][j]=1;
+            }
+            i--;
+            j--;
+            break;
+        case 2:
+            for(;j>=1 && !p[i][j];j--)
+            {
+                printf("%d ",a[i][j]);
+                p[i][j]=1;
+            }
+            i--;
+            j++;
+            break;
+        case 3:
+            for(;i>=1 && !p[i][j];i--)
+            {
+                printf("%d ",a[i][j]);
+                p[i][j]=1;
+            }
+            i++;
+            j++;
+            break;
         }
-        else if(p>t)
-        {
-            p-=small;
-            small++;
-        }
-        else
-        {
-            big++;
-            p+=big;
-        }
+        printf("\n");
+        flag=(flag+1) & 3;
     }
 }
 
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    sequence(n);
+    freopen("in","r",stdin);
+    int n,m;
+    scanf("%d%d",&n,&m);
+    memset(a,0,sizeof(a));
+    memset(p,0,sizeof(a));
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=m;j++)
+            scanf("%d",&a[i][j]);
+    for(int i=0;i<=m;i++)
+    {
+        a[0][i]=1;
+        a[n+1][0]=1;
+    }
+    for(int i=0;i<=n;i++)
+    {
+        a[i][0]=1;
+        a[i][m+1]=1;
+    }
+    printRect(n,m);
     return 0;
 }
